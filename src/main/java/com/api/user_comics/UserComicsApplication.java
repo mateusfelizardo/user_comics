f277e8240daebc250033934a86c248df21ec21bf
Mateus Vieira Felizardo <mateusfeliz159@gmail.com>
2022-02-01 20:55:52 -0300

    commit inicial

---
+++ src/main/java/com/api/user_comics/UserComicsApplication.java
@@ -0,0 +1,13 @@
+package com.api.user_comics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserComicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserComicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/user_comics/UserComicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.user_comics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserComicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
